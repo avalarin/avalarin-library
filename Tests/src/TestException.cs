524fbe97de1271126c9095b5a3de3f207d30c049
aprokopyev <aleksandr1593@gmail.com>
2014-02-04 15:34:43 +0600

    Добавил класс TestException.

---
+++ Tests/src/TestException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Tests {
+    [Serializable]
+    public class TestException : Exception {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public TestException() {
+        }
+
+        public TestException(string message) : base(message) {
+        }
+
+        public TestException(string message, Exception inner) : base(message, inner) {
+        }
+
+        protected TestException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context) {
+        }
+    }
+}
