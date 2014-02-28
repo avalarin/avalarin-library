5885b18b926a4ac6b8716af599a75edf8939a6d1
aprokopyev <aleksandr1593@gmail.com>
2014-02-28 23:34:21 +0600

    Удалил неиспользуемый класс LocalizationConstants.

--- Avalarin.Web.Utils/src/Localization/LocalizationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Avalarin.Web.Localization {
-    public static class LocalizationConstants {
-
-        public const string ErrorsPath = "/shared/errors";
-        public const string CommandsPath = "/shared/commands";
-
-    }
-}
