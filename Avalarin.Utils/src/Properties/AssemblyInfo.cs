110f8c03efc07fd4f2a442567a1aafcfe9922735
aprokopyev <aleksandr1593@gmail.com>
2014-02-28 23:35:24 +0600

    v1.0.4

--- Avalarin.Utils/src/Properties/AssemblyInfo.cs
+++ Avalarin.Utils/src/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d2a798d-7076-45fc-8735-a6e969f58fb7")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
