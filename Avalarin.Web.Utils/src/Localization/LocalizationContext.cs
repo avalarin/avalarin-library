a796332b4403b68024742372ef51fa914091fddd
avalarin <avalarin@gmail.com>
2014-03-06 17:33:15 +0600

    Добавил класс LocalizationContext.

---
+++ Avalarin.Web.Utils/src/Localization/LocalizationContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+
+namespace Avalarin.Web.Localization {
+    public class LocalizationContext : DynamicObject {
+        private readonly CultureInfo _culture;
+        private readonly String _path;
+        private readonly ILocalizationRepository _localizationRepository;
+
+        public LocalizationContext(ILocalizationRepository localizationRepository, CultureInfo culture, string path) {
+            if (localizationRepository == null) throw new ArgumentNullException("localizationRepository");
+            if (culture == null) throw new ArgumentNullException("culture");
+            if (path == null) throw new ArgumentNullException("path");
+
+            _localizationRepository = localizationRepository;
+            _culture = culture;
+            _path = path;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result) {
+            result = Localize(binder.Name);
+            return true;
+        }
+
+        public String this[String key] {
+            get { return Localize(key); }
+        }
+
+        public String Localize(String key) {
+            if (key == null) throw new ArgumentNullException("key");
+            return _localizationRepository.Get(_culture, _path, key);
+        }
+    }
+}
---
+++ Avalarin.Web.Utils/src/Localization/LocalizationRepositoryExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Avalarin.Web.Localization {
+    public static class LocalizationRepositoryExtensions {
+
+        public static LocalizationContext CreateContext(this ILocalizationRepository repository, CultureInfo cultureInfo, string path) {
+            return new LocalizationContext(repository, cultureInfo, path);
+        }
+
+    }
+}
